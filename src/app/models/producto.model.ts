a7c4e9f198688c6453bc532445c3ac85c852f4d1
mephysto0 <blackrootx3@gmail.com>
2021-10-14 15:16:50 -0300

    app: arreglo modulos

---
+++ src/app/models/producto.model.ts
@@ -0,0 +1,7 @@
+export interface Product{
+  _id: string;
+  nombre:string;
+  tienda:string;
+  imgUrl: string;
+  categoria:string;
+}
---
+++ src/app/service/product/PRODUCT.const.ts
@@ -0,0 +1,26 @@
+import { Product } from '../../models/producto.model';
+
+export const PRODUCT: Product[] =
+[
+  {
+    _id: '01',
+    nombre:'Repisa a medida',
+    tienda:'Le Tallere',
+    imgUrl: '/assets/imagenes/le_tallere/producto_1/245334874_158526689815988_2223693560567097557_n.jpg',
+    categoria:'fg',
+  },
+  {
+    _id: '02',
+    nombre:'Tabla para cortar',
+    tienda:'Le Tallere',
+    imgUrl: '/assets/imagenes/le_tallere/producto_1/245503857_2230538940429790_1068097796140139898_n.jpg',
+    categoria:'fg',
+  },
+  {
+    _id: '03',
+    nombre:'Barra para balcon',
+    tienda:'Le Tallere',
+    imgUrl: '/assets/imagenes/le_tallere/producto_1/243582459_275364831115880_5454893860313864524_n.jpg',
+    categoria:'fg',
+  },
+]
