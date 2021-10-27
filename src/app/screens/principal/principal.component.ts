3231fe1eb40f9a7a0004f74e547223b69e2958cf
mephysto0 <blackrootx3@gmail.com>
2021-10-27 19:59:57 -0300

    cambios

--- src/app/screens/principal/principal.component.ts
+++ src/app/screens/principal/principal.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Product } from '../../models/producto.model';
-import { ProductService } from 'src/app/service/product/product.service';
-import {Store} from '../../models/tienda.model';
-import { StoreService } from 'src/app/service/store/store.service';
+
+import { ProductService } from '../../services/product.services'
+import { Product } from '../../models/producto.model'
+
 import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
@@ -13,21 +13,25 @@ import { ActivatedRoute, Params } from '@angular/router';
 export class PrincipalComponent implements OnInit {
 
   public productos : Product[] | undefined;
-  public tiendas: Store [] | undefined;
   public bannerTienda: 'Le tallere' | undefined;
+
+  products: Product[] = [];
 
   constructor(
     private productService: ProductService,
-    private StoreService:StoreService,
     private activatedRoute: ActivatedRoute,
     ){}
 
   ngOnInit(): void {
-    this.productos = this.productService.getAllProduct();
-    this.tiendas = this.StoreService.getAllStore();
 
 
-
+    this.productService.getProducts()
+    .subscribe(
+      res => {
+        this.products = res;
+      },
+      err => console.log(err)
+    )
 
   }
 
