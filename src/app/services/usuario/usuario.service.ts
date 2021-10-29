d077c01ef63c49cd92d74b54f90094d47e6ab50d
Pedro <pedro3_osorio@hotmail.com>
2021-10-29 19:08:23 -0300

    perfil muestra datos del usuario logeado

--- src/app/services/usuario/usuario.service.ts
+++ src/app/services/usuario/usuario.service.ts
@@ -1,11 +1,25 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
+import { UserStore } from 'src/app/models/UserStore.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuarioService {
-  URI = 'http://localhost:4000/private';
+  URI = 'http://localhost:4000/user';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  
+
+  getUser(id: string) {
+    return this.http.get<UserStore>(`${this.URI}/${id}`);
+  }
+
+  deleteUser(id: string) {
+    return this.http.delete(`${this.URI}/${id}`);
+  }
+
+  updateUser(id: string, nombre: string, apellido: string, rut: string, razon_social: string, email: string, password : string) {
+    return this.http.put(`${this.URI}/${id}`, {nombre, apellido,rut,razon_social,email,password });
+  }
 }
