7788ec1c8051a650eab30e4a3922e84fd3cc36cf
mephysto0 <56182028+mephysto0@users.noreply.github.com>
2021-11-15 19:26:37 -0300

    fix: like

--- src/app/component/producto-tarjeta/producto-tarjeta.component.ts
+++ src/app/component/producto-tarjeta/producto-tarjeta.component.ts
@@ -4,6 +4,7 @@ import { ProductService } from 'src/app/services/product.services';
 import { Product} from '../../models/producto.model';
 import { Event, NavigationStart, NavigationEnd, NavigationError } from '@angular/router';
 import { Subscriber } from 'rxjs';
+import { LocalStorageService } from 'src/app/services/localStorage/local-storage.service';
 
 @Component({
   selector: 'app-producto-tarjeta',
@@ -16,32 +17,31 @@ export class ProductoTarjetaComponent implements OnInit {
   @Input()
   public producto!: Product;
 
-  isActive : boolean | undefined;
+  isActive : boolean | any;
 
 
   constructor(
     private productService: ProductService,
     private router: Router,
+    private localstorage : LocalStorageService,
     ) {
    }
 
   ngOnInit(): void {
 
-    this.isActive = false;
+
+    const aux2 = this.productService.isliked(this.productService.getUser());
+    this.isActive = aux2;
+    console.log(this.productService.isliked(this.productService.getUser()))
+    console.log(this.productService.getUser())
   }
 
 
 	onClick() {
+
 		this.producto.likes += (this.isActive) ? -1 : 1;
 		this.isActive = !this.isActive;
     this.productService.likeProduct(this.producto._id).subscribe();
 	}
-
-  like(): void{
-
-    //window.location.reload();
-  }
-
-
 
 }
