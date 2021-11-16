abc3c2e643f666f4331f329611fba15d72ad4781
mephysto0 <56182028+mephysto0@users.noreply.github.com>
2021-11-16 13:13:40 -0300

    likeeee

--- src/app/component/producto-tarjeta/producto-tarjeta.component.ts
+++ src/app/component/producto-tarjeta/producto-tarjeta.component.ts
@@ -17,8 +17,8 @@ export class ProductoTarjetaComponent implements OnInit {
   @Input()
   public producto!: Product;
 
-  isActive : boolean | any;
-
+  isActive : true | any;
+  valor : any;
 
   constructor(
     private productService: ProductService,
@@ -28,16 +28,22 @@ export class ProductoTarjetaComponent implements OnInit {
    }
 
   ngOnInit(): void {
+ // console.log(this.valor.unliked)
 
-    if(this.productService.isliked(this.producto._id) === true) {
-      this.isActive = true;
-      console.log(this.isActive)
+    this.productService.isliked(this.producto._id).subscribe((valor: any) => {
+      this.valor = valor;
+    if(this.valor.unliked === false) {
+      this.isActive = false;
+      console.log('verdad')
     }
     else {
-      this.isActive = false;
-      console.log(this.isActive)
+      this.isActive = true;
+      console.log('falsd')
     }
+    })
+
   }
+
 
 
 	onClick() {
