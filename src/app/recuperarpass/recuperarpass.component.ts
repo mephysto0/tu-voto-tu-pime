4045ea5bca0a412cd5bbac1587f4444c05c60ed5
Pedro <pedro3_osorio@hotmail.com>
2021-11-19 01:44:48 -0300

    sector admin

--- src/app/recuperarpass/recuperarpass.component.ts
+++ src/app/recuperarpass/recuperarpass.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { UsuarioService } from '../services/usuario/usuario.service';
 
 @Component({
   selector: 'app-recuperarpass',
@@ -7,7 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecuperarpassComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    
+  ) { 
+    
+  }
 
   ngOnInit(): void {
   }
